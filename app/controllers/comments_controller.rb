ed3e9b5f19bade715e9cc3fd525fbd2057fc1111
Andrew Chen <andrewchen747@gmail.com>
2019-11-20 00:48:35 -0800

    Starting to create comment functionality.

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,11 @@
+class CommentsController < ApplicationController
+	def create
+		@post = Post.find(params[:id])
+		@comment = @post.comments.create(comment_params)
+		redirect_to post_path(@post)
+	end
+	
+	private comment_params
+		params.require(:comment).permit(:username, :body)
+	end	
+end
