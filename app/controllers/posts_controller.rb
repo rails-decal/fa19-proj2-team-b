e902e7aad7562e1e6e2244dd970603cd40095a4f
suwinnie <winniebsu@gmail.com>
2019-12-01 21:38:41 -0800

    active storage feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -49,6 +49,6 @@ class PostsController < ApplicationController
 	end
 
 	private def post_params
-		params.require(:post).permit(:title, :body)
+		params.require(:post).permit(:title, :body, :photo)
 	end
 end
